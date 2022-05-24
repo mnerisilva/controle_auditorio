983ea8f664ab50404d2e82baa3b81aff62af676a
mnerisilva <marceloneri@gmail.com>
2022-05-24 10:45:35 -0300

    update

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ console.log(_poltrona);
 
 _capacidade.textContent = _cap;
 
+const _body = document.querySelector('body');
+const _modal = document.querySelector('.modal');
 const _svg = document.querySelector('svg');
 console.log(_svg);
 console.log(_svg.getAttribute('width'));
 
+// desabilita menu de contexto do elemento svg
+        _svg.addEventListener('contextmenu', e => {
+        e.preventDefault();
+        });
+//
 
 
 _poltrona.forEach((conteudo, item) => {
@@ -19,20 +26,28 @@ _poltrona.forEach((conteudo, item) => {
     conteudo.addEventListener('mousedown', function(e){
         console.log(e.buttons);
         let elemento = e.target;
-    
-        e.target.classList.toggle("ativa")
-    
-        if(elemento.classList.contains('ativa')){
-            console.log('sim')
-            _cap++
-        }else {
-            console.log('não')
-            _cap--
+        if(e.buttons === 1){ // somente qdo for clicado o botão esquerdo do mouse
+            e.target.classList.toggle("ativa")
+        
+            if(elemento.classList.contains('ativa')){
+                console.log('sim')
+                _cap++
+            }else {
+                console.log('não')
+                _cap--
+            }
+                
+            _capacidade.textContent = _cap;
+        
+            console.log(elemento);
+            //console.log(e.pageX, e.pageY);
+            console.log(e.pageX, e.pageY);
+            console.log(e.offsetLeft, e.pageY);
+            _modal.style.pageX = _body.pageX;
+            _modal.style.pageY = _body.pageY;
+            _modal.style.display = "block";
+
         }
-            
-        _capacidade.textContent = _cap;
-    
-        console.log(elemento);
     })
 
 });

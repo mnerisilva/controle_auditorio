40f1bbd062866ef66d8eb86238a69f9fc5bd6f1b
mnerisilva <marceloneri@gmail.com>
2022-05-24 15:44:46 -0300

    update

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ _score.style.display = "none";
 console.log(_svg);
 console.log(_svg.getAttribute('width'));
 
+let _tipo = 1;
 
 const _canvas = document.querySelector("#myCanvas");
 
@@ -31,11 +32,17 @@ ctx.moveTo(20, 20);
 ctx.lineTo(200, 20);
 ctx.stroke();*/
 
-ctx.fillStyle = "brown";
+ctx.fillStyle = "white";
 ctx.font = '20px serif';     
-ctx.fillText("COTAÇÃO: ", 10, 30);
+ctx.fillText("LOTAÇÃO: ", 10, 30);
 
 
+function getValue(radio) {
+    console.log(radio.value);
+    if(radio.value === "1"){_tipo = 1}
+    if(radio.value === "2"){_tipo = 2}
+    if(radio.value === "3"){_tipo = 3}
+}
 
 
 
@@ -53,12 +60,17 @@ _poltrona.forEach((conteudo, item) => {
         console.log(e.buttons);
         let elemento = e.target;
         if(e.buttons === 1){ // somente qdo for clicado o botão esquerdo do mouse
-            e.target.classList.toggle("ativa")
+            e.target.classList.toggle("on")
+
+            //if(_tipo == 1){e.target.classList.add("")}
+            if(_tipo == 2){e.target.classList.add('acessibilidade');}
+            if(_tipo == 3){e.target.classList.add('estudante');}
+            console.log(_tipo);
 
             ctx.clearRect(0,0,600,600); 
             console.log(_width, _height);
         
-            if(elemento.classList.contains('ativa')){
+            if(elemento.classList.contains('on')){
                 console.log('sim')
                 _cap++
             }else {
@@ -75,7 +87,7 @@ _poltrona.forEach((conteudo, item) => {
             _modal.style.pageX = _body.pageX;
             _modal.style.pageY = _body.pageY;
             _modal.style.display = "block";         
-            ctx.fillText("COTAÇÃO: ", 10, 30);
+            ctx.fillText("LOTAÇÃO: ", 10, 30);
             ctx.font = '26px serif'; 
             ctx.fillText(_cap, 150, 30);
             ctx.font = '20px serif'; 

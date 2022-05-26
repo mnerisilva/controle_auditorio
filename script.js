4863311191a0054102c0686b7fc9d4dcf8d66e4d
mnerisilva <marceloneri@gmail.com>
2022-05-26 16:44:38 -0300

    update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const _zoom_button_menos = document.querySelector(".zoom-button-menos");
 const _myCanvas = document.querySelector(".myCanvas");
 
 const _capacidade_total = document.querySelector('table .cap-tot');
-const _capacidade_atual = document.querySelector('table .cap-atu');
+const _capacidade_atual = document.querySelector('table .cap-atu span');
 
 const arrIdPoltronas = [,"costas_p01", "costas_p02", "costas_p03", "costas_p04", "costas_p05", "costas_p06", "costas_p07", "costas_p08", "costas_p09", "costas_p10", "costas_p11", "costas_p12", "costas_p13", "costas_p14", "costas_p15", "costas_p16", "costas_p17", "costas_p18", "costas_p19", "costas_p20", "costas_p21", "costas_p22", "costas_p23", "costas_p24", "costas_p25", "costas_p26", "costas_p27", "costas_p28", "costas_p29", "costas_p30", "costas_p31", "costas_p32", "costas_p33", "costas_p34", "costas_p35", "costas_p36", "costas_p37", "costas_p38", "costas_p39"];
 
